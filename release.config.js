0ea3b92f1839a4e5ea39bd9876c064bc7a1086bb
Imran Iqbal <iqbalmy@hotmail.com>
2021-05-09 06:26:57 +0100

    ci(release.config.js): ensure push is to forked repo

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  branch: 'master',
-  repositoryUrl: 'https://github.com/saltstack-formulas/apt-formula',
+  branch: 'antora',
+  repositoryUrl: 'https://github.com/myii/apt-formula',
   plugins: [
       ['@semantic-release/commit-analyzer', {
         preset: 'angular',
